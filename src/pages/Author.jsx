61bd13f7f4c9eb19804a756f283ba3ee88432dff
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-07-20 10:33:15 -0500

    fetching data

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const [author, setAuthor] = useState([]);
+  const [skeleton, setSkeleton] = useState(true);
+  const { authorId } = useParams();
+
+  
+  useEffect(() => {
+    async function fetchAuthor() {
+      const { data } = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+      );
+      setAuthor(data);
+      setSkeleton(false);
+      console.log(data)
+    }
+    fetchAuthor();
+  }, [authorId]);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -22,35 +41,64 @@ const Author = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                {skeleton ? (
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <Skeleton borderRadius={100} height={150} width={150} />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            <Skeleton height={24} width={200} />
+                            <span className="profile_username">
+                              <Skeleton height={16} width={100} />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton height={16} width={250} />
+                            </span>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">573 followers</div>
+                        <Skeleton height={40} width={150} />
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
+                ) : (
+                  <div className="d_profile de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_avatar">
+                        <img src={author.authorImage} alt="" />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            Monica Lucas
+                            <span className="profile_username">{author.tag}</span>
+                            <span id="wallet" className="profile_wallet">
+                              UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">573 followers</div>
+                        <Link to="#" className="btn-main">
+                          Follow
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
 
               <div className="col-md-12">
